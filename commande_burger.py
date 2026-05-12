bd7c9f8ff928c28420eb9e1fe8ae158e38681433
monmirail6-dev <monmirail6@gmail.com>
2026-05-12 14:35:15 +0200

    Update commande_burger.py

--- commande_burger.py
+++ commande_burger.py
@@ -204,10 +204,7 @@ if st.session_state.admin_mode:
         
     # --- ajouter burger ---
     with st.sidebar.expander("➕ ajouter Burger", expanded=False):
-        new_burger = st.text_input("nouveau burger", key="new_burger")
-        price = st.number_input("prix", min_value=0, step=0.01, key="new_price")
 
-        
             
             
 # ============================================================
