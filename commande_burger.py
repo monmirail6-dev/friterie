21687f167085e4a7f1e8d978640bd04151acf55a
monmirail6-dev <monmirail6@gmail.com>
2026-05-12 11:05:10 +0200

    Update commande_burger.py

--- commande_burger.py
+++ commande_burger.py
@@ -115,15 +115,12 @@ if st.session_state.user_id:
 )
 
     with st.container():
-    st.markdown('<div class="welcome-box">', unsafe_allow_html=True)
-
-        col1, col2 = st.columns([1, 6])
-
-        with col1:
-            st.image("logo.png", width=70)
-
-        with col2:
-            st.markdown(
+        st.markdown('<div class="welcome-box">', unsafe_allow_html=True)
+            col1, col2 = st.columns([1, 6])
+            with col1:
+                st.image("logo.png", width=70)
+            with col2:
+                st.markdown(
             f"""
             <h1 style="
                 font-size:42px;
